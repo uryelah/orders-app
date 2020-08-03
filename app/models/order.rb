f38a533d0a927cff3143175b08c3efa878542cd8
sarah <sarachamorro@gmail.com>
2020-08-03 14:08:35 -0300

    Add validations to state and control_number columns

--- app/models/order.rb
+++ app/models/order.rb
@@ -1,2 +1,4 @@
 class Order < ApplicationRecord
+  validates :state, presence: true, numericality: { only_integer: true }
+  validates :control_number, presence: true, numericality: { only_integer: true }
 end
