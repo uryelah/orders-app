cb233edbde8edeea651ee654a2a8b4debf3d8a20
sarah <sarachamorro@gmail.com>
2020-08-03 15:25:45 -0300

    Add tests to Order model

--- app/models/order.rb
+++ app/models/order.rb
@@ -1,4 +1,24 @@
 class Order < ApplicationRecord
   validates :state, presence: true, numericality: { only_integer: true }
-  validates :control_number, presence: true, numericality: { only_integer: true }
+  validates :control_number, allow_blank: true, numericality: { only_integer: true }, uniqueness: true
+  validate :ensure_unique_control_number
+
+  before_validation :set_initial_state
+  after_create :set_control_number
+
+  protected
+
+  def set_initial_state
+    self.state = 0
+  end
+
+  def set_control_number
+    self.control_number = id if control_number.nil? || control_number.zero?
+  end
+
+  def ensure_unique_control_number
+    return unless Order.where(control_number: control_number).length.positive?
+
+    errors.add(:control_number, 'Control_number should be unique')
+  end
 end
--- spec/models/order_spec.rb
+++ spec/models/order_spec.rb
@@ -1,5 +1,40 @@
 require 'rails_helper'
 
 RSpec.describe Order, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it 'is valid with valid attributes' do
+    order = Order.new(control_number: 10)
+    expect(order).to be_valid
+  end
+
+  it 'is valid with no attributes' do
+    order = Order.new
+    expect(order).to be_valid
+  end
+
+  it 'is valid without a control_number' do
+    order = Order.new(control_number: nil)
+    expect(order).to be_valid
+  end
+
+  it 'is not valid with a non integer control_number' do
+    order = Order.new(control_number: 'control')
+    expect(order).to_not be_valid
+  end
+
+  it 'is not valid when control_number is not unique' do
+    Order.create(control_number: 10)
+    order = Order.new(control_number: 10)
+    expect(order).to_not be_valid
+  end
+
+  it 'assigns 0 as the state value before validation' do
+    order = Order.new(control_number: 10)
+    order.valid?
+    expect(order.state).to be(0)
+  end
+
+  it 'assigns it\'s id as control_number if no control_number is given' do
+    order = Order.create
+    expect(order.control_number).to be(order.id)
+  end
 end
