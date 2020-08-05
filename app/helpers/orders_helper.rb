e13510b11ccdafa69a92dde14e45a97b215d5f10
sarah <sarachamorro@gmail.com>
2020-08-05 00:03:22 -0300

    Refactor index view code into partials and helpers

--- app/helpers/orders_helper.rb
+++ app/helpers/orders_helper.rb
@@ -1,4 +1,16 @@
 # frozen_string_literal: true
 
+# Orders helper methods
 module OrdersHelper
+  def number_to_state(index)
+    %w[Pending Progress Complete][index]
+  end
+
+  def number_to_class(index)
+    %w[warning info success][index]
+  end
+
+  def btn_disabled(state)
+    state == 2 ? 'disabled' : ''
+  end
 end
